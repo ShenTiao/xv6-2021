7ad200cc829a4e4355a9d99910f6ff9bf814848d
ShenTiao <584892986@qq.com>
2021-10-10 18:49:51 -0700

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,41 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+    int parentFd[2];
+    int childFd[2];
+
+    pipe(parentFd);
+    pipe(childFd);
+
+    int pid=fork();
+    if(pid==0){
+        //child process
+        close(parentFd[0]);
+        close(childFd[1]);
+        char buf[4];
+        read(childFd[0],buf,4);
+        close(childFd[0]);
+        fprintf(1,"%d: received ping\n",getpid());
+        write(parentFd[1],"pong",4);
+        close(parentFd[1]);
+        exit(0);
+    }
+    else if(pid>0){
+        char buf[4];
+        //文件描述符
+        close(childFd[0]);
+        close(parentFd[1]);
+        write(childFd[1],buf,4);
+        close(childFd[1]);
+        read(parentFd[0],buf,4);
+        close(parentFd[0]);
+        fprintf(1,"%d: received pong\n",getpid());
+        wait(&pid);//等待子进程
+    }
+    else{
+        exit(1);
+    }
+    exit(0);
+}
